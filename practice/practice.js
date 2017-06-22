67a480a20eaf3f77ead3798c034a18ddf65e675f
Hack Reactor Student <student@hackreactor.com>
2017-06-22 14:30:59 -0500

    MEssagesdfskajdfhgla

--- practice/practice.js
+++ practice/practice.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+  $("form").submit(function(event){
+    event.preventDefault();
+    var name = $("#name").val();
+    console.log(name);
+    var comment = $("#comment").val();
+    console.log(comment);
+  })
 })
--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
-  // Your javascript goes here
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var name = $("#input-name").val();
+    console.log(name);
+    var message = $("#input-message").val();
+    console.log(message);  
+    $("#message-board").append("<li class='message-box'><span class='name'>[bob]: </span>[Hello is this thing on]</li>");
+      
+
+  })
  
 });
