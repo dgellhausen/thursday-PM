7d9ade30a202ed3a309f44dd3b0f9315f408405b
Hack Reactor Student <student@hackreactor.com>
2017-06-22 15:56:06 -0500

    Message Board

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ $(document).ready(function(){
   $("#input-form").submit(function(event){
     event.preventDefault();
     var name = $("#input-name").val();
-    $(".name").append(name + ":");
     var message = $("#input-message").val();
-    $("span").append(message);
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    $("ul").append("<li class='message-box'>"+ name + ": " + message+ "....." + dateTime+"</li>");
   })
+  $("#clear-button").click(function(){
+    $("#input-name").val("");
+    $("#input-message").val("");
+  // $("#clear-button").click(function(){
+  //   $(".center").val("");
+  })
+  
   
 });
