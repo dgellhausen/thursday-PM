e852c50a11b56012e38c320a62951db3b8d80fb3
Hack Reactor Student <student@hackreactor.com>
2017-06-22 14:31:20 -0500

    tried to get function working

--- practice/practice.js
+++ practice/practice.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+  $("form").submit(function(event){
+    event.preventDefault();
+    var name = $("#name").val();
+    console.log("the form was submitted")
+    console.log(name);
+    var message = $("#comment").val();
+    console.log(message);
+  })
 })
--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
-  // Your javascript goes here
- 
+  var form = $("#message-form")
+  var nl = document.getElementById("nl");
+  console.log(form)
+  form.submit(function(event){
+    event.preventDefault();
+    var name = $("#input-name").val();
+    console.log(name);
+    var message = $("#input-message").val();
+    console.log(message);
+    function ht(){
+      var newnlItem = document.createElement("li");
+      var nlValue = document.createTextNode(i); 
+       newnlItem.appendChild(nlValue);
+      nl.appendChild(newnlItem);
+    }
+  })
+
+
 });
