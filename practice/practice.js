0b55b8809ab53a3834e7ea4c754e3f63aace9342
Hack Reactor Student <student@hackreactor.com>
2017-06-22 13:59:34 -0500

    practice

--- practice/practice.js
+++ practice/practice.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-
+  $(document).ready(function(event){
+    $("form").submit(function() {
+  console.log("the form was submitted")
+    event.preventDefault();
+    var name = $("#name").val();
+    console.log(name);
+    event.preventDefault();
+    var name = $("#name").val();
+    var comment = $("#comment").val();
+    console.log(comment);
+    })
 })
