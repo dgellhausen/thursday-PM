f3505836a8da5f5f3cc3da9b966da991511c8503
Hack Reactor Student <student@hackreactor.com>
2017-06-22 16:39:30 -0500

    Finished up message board website

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ $(document).ready(function(){
     console.log("The form was submitted");
     var name = $("#input-name").val();
     var message = $("#input-message").val()
+    var today = new Date();
+    var date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" +today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     console.log("Name: " + name);
     console.log("Message: " + message);
-    $("#message-box").append("<li class='message-box'><span class='name'>" + name + ": </span>" + message + "</li>")
+    $("#message-box").append("<li class='message-box'><span class='name'>" + name + ": " + "</span>" + message + " " + date + " \xa0\xa0" + time + "</li>")
+    $("#input-name").val("");
+    $("#input-message").val("");
   })
   $("#clear").click(function(){
-    $(".message-box").val("")
+    $("#input-name").val("");
+    $("#input-message").val("");
   })
 });
