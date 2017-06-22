e036e3635827f7b9d6ec9ca2c96d14ab8490f393
Hack Reactor Student <student@hackreactor.com>
2017-06-22 14:30:50 -0500

    Created a message board

--- practice/practice.js
+++ practice/practice.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+  $("form").submit(function(event){
 
+    event.preventDefault();
+
+    var name = $("#name").val();
+    console.log("Name: " + name);
+
+    var comment = $("#comment").val();
+    console.log("Comment: " + comment);
+
+    console.log("The form was submitted");
+
+  })
 })
--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
   // Your javascript goes here
- 
+  console.log('Page is loaded')
+  var form = $("#message-form")
+  console.log(form);
+  $(form).submit(function(event){
+    event.preventDefault();
+    console.log("The form was submitted");
+    var name = $("#input-name").val();
+    var message = $("#input-message").val()
+    console.log("Name: " + name);
+    console.log("Message: " + message);
+    $("#message-box").append("<li class='message-box'><span class='name'>" + name + ": </span>" + message + "</li>")
+  })
+  $("#clear").click(function(){
+    $(".message-box").val("")
+  })
 });
