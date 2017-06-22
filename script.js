902df7d31d0149499ce707af368446968b108cbe
Hack Reactor Student <student@hackreactor.com>
2017-06-22 15:06:06 -0500

    Added submitted at this date

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ $(document).ready(function(){
   $(form).submit(function(event){
     event.preventDefault();
     console.log("the form was submitted");
+    // Declaring variables
     var name = $("#input-name").val();
     var message = $("#input-message").val();
     var postName = name + ": ";
     var newPostText = name + ": " + message;
+    var today = new Date();
+    var date = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
+    var time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+    var completeTime = `${date} ${time}`;
+    console.log(`${date} ${time}`);
+    //
     if (name != "" && message != ""){
-      var newPost = $("ul").append("<li class='message-box'><span class='name'>" + postName + "</span>" + message + "</li>");
+      var newPost = $("ul").append(`<li class='message-box'><span class='name'> ${postName} </span> ${message} (submitted at ${date})</li>`);
       $(".center").val("");
     }
   });
