b38f34fe2074424da9036b3373824bf002478d2d
Hack Reactor Student <student@hackreactor.com>
2017-06-22 14:31:04 -0500

    message board

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
   // Your javascript goes here
- 
+ $("form").submit(function(event) {
+    event.preventDefault();
+    var name = $("#input-name").val();
+    var comment = $("#input-message").val();
+    console.log(comment);
+  });
 });
