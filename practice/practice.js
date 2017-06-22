bc43019c2994576e151f5fa12b138b42fc5dd289
Hack Reactor Student <student@hackreactor.com>
2017-06-22 14:32:43 -0500

    Completed thursday-PM lesson and added extra 'fancy' stuff

--- practice/practice.js
+++ practice/practice.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+  $("form").submit(function(event){
+    event.preventDefault();
+    var name = $("#name").val();
+    var comment = $("#comment").val()
+    console.log(comment);
+  });
 })
--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
   // Your javascript goes here
- 
+  console.log("page is loaded");
+  var form = $("#message-form");
+  console.log(form);
+  $(form).submit(function(event){
+    event.preventDefault();
+    console.log("the form was submitted");
+    var name = $("#input-name").val();
+    var message = $("#input-message").val();
+    var postName = name + ": ";
+    var newPostText = name + ": " + message;
+    if (name != "" && message != ""){
+      var newPost = $("ul").append("<li class='message-box'><span class='name'>" + postName + "</span>" + message + "</li>");
+      $(".center").val("");
+    }
+  });
 });
