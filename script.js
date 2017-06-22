16348bb2b1e84f9e8eee30a68da8f47945caa1b0
Hack Reactor Student <student@hackreactor.com>
2017-06-22 14:32:25 -0500

    Message

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
   // Your javascript goes here
- 
+  console.log("page is loaded")
+  $("#input-form").submit(function(event){
+    event.preventDefault();
+    var name = $("#input-name").val();
+    $(".name").append(name + ":");
+    var message = $("#input-message").val();
+    $("span").append(message);
+  })
+  
 });
