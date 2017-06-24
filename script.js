ee6df4e7baa0d535d14e3621c8069ef9d781bfca
Hack Reactor Student <student@hackreactor.com>
2017-06-24 11:00:21 -0500

    Did what Lincoln wanted

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
     var message = $("#input-message").val();
     var postName = name + ": ";
     var newPostText = name + ": " + message;
+    var newLogMessage = name
     var today = new Date();
     var date = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
     var time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
@@ -18,8 +19,9 @@ $(document).ready(function(){
     console.log(`${date} ${time}`);
     //
     if (name != "" && message != ""){
-      var newPost = $("ul").append(`<li class='message-box'><span class='name'> ${postName} </span> ${message} (posted at ${date})</li>`);
+      var newPost = $("#boardmessage").append(`<li class='message-box'><span class='name'> ${postName} </span> ${message} (posted at ${date})</li>`);
       $(".center").val("");
+      var newLog = $("#loglist").append(`<li class="log-box"><span class="name"> ${newLogMessage} has joined the room</li>`)
     }
   });
 });
