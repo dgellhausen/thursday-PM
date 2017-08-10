cfb5c69a1575918bb0f031145d2eed1f03398e03
Hack Reactor Student <student@hackreactor.com>
2017-08-10 11:10:15 -0500

    made it so it saves posts

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
   // Your javascript goes here
- 
+  console.log("page is loaded");
+  var form = $("#message-form");
+  console.log(form);
+
+  $("#message-form").submit(function(){
+    event.preventDefault();
+    console.log("The form has been submitted")
+    var name = $( "#input-name" ).val();
+    var message = $( "#input-message" ).val();
+    console.log(name);
+    console.log(message);
+    $( "#message-board" ).append(`<li class='message-box'><span class='name'> `+ name+ `: </span>`+message+`</li>`);
+  });
+
 });
